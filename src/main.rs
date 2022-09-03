e1ff4e1f0d52ff9db1e14a1b846d961a04a7a0e3
thonpa <thonpa@neko.neko.org>
2022-09-03 11:32:01 -0400

    Added emitters, and implemented them

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // use emitter::Emitter;
 use fluid::Fluid;
+use emitter::Emitter;
 use fluid_math::*;
 use macroquad::prelude::*;
 
@@ -49,24 +50,24 @@ async fn main() {
     let mut fluid_red: u8 = 0xff;
     let mut fluid_green: u8 = 0xff;
     let mut fluid_blue: u8 = 0xff;
-    // let mut visc_slider_emitter = 0.0000001;
-    // let mut diffusion_slider_emitter = 0.0000001;
-    // let mut density_slider_emitter = 100.;
-    // let mut speed_slider_emitter: f32 = 0.15;
-    // let mut fluid_red_emitter: u8 = 0xff;
-    // let mut fluid_green_emitter: u8 = 0xff;
-    // let mut fluid_blue_emitter: u8 = 0xff;
+    let mut visc_slider_emitter = 0.0000001;
+    let mut diffusion_slider_emitter = 0.0000001;
+    let mut density_slider_emitter = 100.;
+    let mut speed_slider_emitter: f32 = 0.15;
+    let mut fluid_red_emitter: u8 = 0xff;
+    let mut fluid_green_emitter: u8 = 0xff;
+    let mut fluid_blue_emitter: u8 = 0xff;
     let mut fluid = Fluid::new(
         0.15,
         diffusion_slider,
         visc_slider,
         (fluid_red, fluid_green, fluid_blue),
     );
-    // let emitter: &mut Emitter = &mut Emitter::empty();
+    let emitter: &mut Emitter = &mut Emitter::empty();
     loop {
         cap_fps(60);
 
-        // emitter.step();
+        emitter.step();
         fluid.step();
         if pmouse == (0, 0) {
             pmouse = (mouse_position().0 as i32, mouse_position().1 as i32);
@@ -84,24 +85,24 @@ async fn main() {
                 ((mouse_position().1 as i32 / SCALE) - (pmouse.1 / SCALE)) as f32,
             );
         }
-        // if is_mouse_button_pressed(MouseButton::Right) {
-        //     *emitter = Emitter::new(
-        //         mouse_position().0 as i32,
-        //         mouse_position().1 as i32,
-        //         mouse_position().0 as i32,
-        //         mouse_position().1 as i32 - 100,
-        //         (fluid_red_emitter, fluid_green_emitter, fluid_blue_emitter),
-        //         diffusion_slider_emitter,
-        //         visc_slider_emitter,
-        //         0.15,
-        //         density_slider_emitter,
-        //     );
-        // }
+        if is_mouse_button_pressed(MouseButton::Right) {
+            *emitter = Emitter::new(
+                mouse_position().0 as i32,
+                mouse_position().1 as i32,
+                mouse_position().0 as i32,
+                mouse_position().1 as i32 - 100,
+                (fluid_red_emitter, fluid_green_emitter, fluid_blue_emitter),
+                diffusion_slider_emitter,
+                visc_slider_emitter,
+                0.15,
+                density_slider_emitter,
+            );
+        }
         if is_key_pressed(KeyCode::Backspace) {
             fluid = Fluid::new(0.15, 0.0, visc_slider, (fluid_red, fluid_green, fluid_blue));
         }
         fluid.render_d();
-        // emitter.draw();
+        emitter.draw();
         egui_macroquad::ui(|egui_ctx| {
             egui::Window::new("User").show(egui_ctx, |ui| {
                 ui.add(egui::Slider::new(&mut visc_slider, 0.0..=0.00005).text("Viscosity"));
@@ -116,38 +117,38 @@ async fn main() {
                         Fluid::new(0.15, 0.0, visc_slider, (fluid_red, fluid_green, fluid_blue));
                 }
             });
-            // egui::Window::new("Emitter").show(egui_ctx, |ui| {
-            //     ui.add(
-            //         egui::Slider::new(&mut visc_slider_emitter, 0.0..=0.00005).text("Viscosity"),
-            //     );
-            //     ui.add(
-            //         egui::Slider::new(&mut diffusion_slider_emitter, 0.0..=0.00005)
-            //             .text("Diffusion"),
-            //     );
-            //     ui.add(egui::Slider::new(&mut speed_slider_emitter, 0.0..=1.).text("Speed"));
-            //     ui.add(
-            //         egui::Slider::new(&mut density_slider_emitter, 0.0..=2000.).text("Emission"),
-            //     );
-            //     ui.add(egui::Slider::new(&mut fluid_red_emitter, 0..=255).text("R"));
-            //     ui.add(egui::Slider::new(&mut fluid_green_emitter, 0..=255).text("G"));
-            //     ui.add(egui::Slider::new(&mut fluid_blue_emitter, 0..=255).text("B"));
-            //     if ui.button("Remove").clicked() {
-            //         *emitter = Emitter::empty();
-            //     }
-            // });
+            egui::Window::new("Emitter").show(egui_ctx, |ui| {
+                ui.add(
+                    egui::Slider::new(&mut visc_slider_emitter, 0.0..=0.00005).text("Viscosity"),
+                );
+                ui.add(
+                    egui::Slider::new(&mut diffusion_slider_emitter, 0.0..=0.00005)
+                        .text("Diffusion"),
+                );
+                ui.add(egui::Slider::new(&mut speed_slider_emitter, 0.0..=1.).text("Speed"));
+                ui.add(
+                    egui::Slider::new(&mut density_slider_emitter, 0.0..=2000.).text("Emission"),
+                );
+                ui.add(egui::Slider::new(&mut fluid_red_emitter, 0..=255).text("R"));
+                ui.add(egui::Slider::new(&mut fluid_green_emitter, 0..=255).text("G"));
+                ui.add(egui::Slider::new(&mut fluid_blue_emitter, 0..=255).text("B"));
+                if ui.button("Remove").clicked() {
+                    *emitter = Emitter::empty();
+                }
+            });
         });
         fluid.set_viscosity(visc_slider);
         fluid.set_diffusion(diffusion_slider);
         fluid.set_color((fluid_red, fluid_green, fluid_blue));
         fluid.set_speed(speed_slider);
 
-        // emitter.fluid.set_viscosity(visc_slider_emitter);
-        // emitter.fluid.set_diffusion(diffusion_slider_emitter);
-        // emitter
-        //     .fluid
-        //     .set_color((fluid_red_emitter, fluid_green_emitter, fluid_blue_emitter));
-        // emitter.set_density(density_slider_emitter);
-        // emitter.fluid.set_speed(speed_slider_emitter);
+        emitter.fluid.set_viscosity(visc_slider_emitter);
+        emitter.fluid.set_diffusion(diffusion_slider_emitter);
+        emitter
+            .fluid
+            .set_color((fluid_red_emitter, fluid_green_emitter, fluid_blue_emitter));
+        emitter.set_density(density_slider_emitter);
+        emitter.fluid.set_speed(speed_slider_emitter);
         egui_macroquad::draw();
         draw_fps(&mut fps_counter, &mut fps_text);
         pmouse = (mouse_position().0 as i32, mouse_position().1 as i32);
